0430cc934fc1e0b65b676c2ca46487f164ba80f7
narekye <yegoryan.narek@gmail.com>
2017-02-27 17:11:54 -0600

    Binary search added

---
+++ Binary_Search/Binary_Search/BinaryArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+namespace Binary_Search
+{
+    class BinaryArray
+    {
+        private int[] data;
+        private Random generator = new Random();
+        // Constructor
+        public BinaryArray(int size)
+        {
+
+            data = new int[size];
+            for (int j = 0; j < data.Length; j++)
+            {
+                data[j] = generator.Next(10, 100);
+            }
+
+        }
+
+        // Binary search perform.
+        public int BinarySearch(int searchElement)
+        {
+            int low = 0, high = data.Length - 1, middle = (low + high) / 2, location = -1;
+            do
+            {
+                Write(RemainingElements(low, high));
+                WriteLine(new string('-', 40));
+                if (searchElement == data[middle])
+                {
+                    location = middle;
+                }
+                else if (searchElement < data[middle])
+                {
+                    high = middle - 1;
+                }
+                else
+                {
+                    low = middle + 1;
+                }
+                middle = (low + high + 1) / 2;
+
+            } while ((low <= high) && (location == -1));
+
+            return location;
+        }
+        // shows which count elements remaining.
+        public string RemainingElements(int low, int high)
+        {
+            string temporary = string.Empty;
+            // output spaces alignment
+            for (int i = 0; i < low; i++)
+            {
+                temporary += "    ";
+            }
+
+            // output elements left in array 
+            for (int i = low; i <= high; i++)
+            {
+                temporary += data[i] + " ";
+            }
+            temporary += "\n";
+            return temporary;
+        }
+
+    }
+}
---
+++ Binary_Search/Binary_Search/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main()
+        {
+            int searchInt;
+            int position;
+            BinaryArray searchArray = new BinaryArray(15);
+            searchInt = int.Parse(ReadLine());
+            while (searchInt != -1)
+            {
+                // use binary search to try to find integer
+                position = searchArray.BinarySearch(searchInt);
+
+                // return value of -1 indicates integer was not found
+                if (position == -1)
+                {
+                    WriteLine("The integer {0} was not found.\n", searchInt);
+                }
+                else
+                {
+                    WriteLine("The integer {0} was found in position {1}.\n", searchInt, position);
+                }
+
+                // Prompt and input next int from user 
+                WriteLine("Enter an integer value (-1 to quit): ");
+                searchInt = Convert.ToInt32(Console.ReadLine());
+                WriteLine();
+
+            }
+        }
+    }
+}
