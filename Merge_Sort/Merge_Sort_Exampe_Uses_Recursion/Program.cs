b4d54c3915bb2079ab494618c5b7ce5d72121d64
narekye <yegoryan.narek@gmail.com>
2017-05-06 15:37:29 -0600

    :v: cuting the array :v:

---
+++ Merge_Sort/Merge_Sort_Exampe_Uses_Recursion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Sort_Exampe_Uses_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 7, 8, 10, 6, 5 };
+            MergeSort(array);
+            Console.Read();
+        }
+
+        static void MergeSort(int[] array)
+        {
+            if (array.Length <= 1) return;
+
+            int leftsize = array.Length / 2;
+            int rightsize = array.Length - leftsize;
+
+            int[] left = new int[leftsize];
+            int[] right = new int[rightsize];
+
+            Array.Copy(array, 0, left, 0, leftsize);
+            Array.Copy(array, leftsize, right, 0, rightsize);
+
+            MergeSort(left);
+            MergeSort(right);
+        }
+
+
+    }
+}
